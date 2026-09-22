0f2bf6bc8a34001d1b5386828251c148a39b54a5
Thaddea4 <adeyasyie@gmail.com>
2026-09-22 11:31:16 +0700

    menambahkan banyak kondisi

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
 nilai = float(input("Enter student nilai: "))
+
+if nilai >= 90:
+    print("Excellent performance")
+elif nilai >= 80:
+    print("Very Good Performance")
+elif nilai >= 70:
+    print("Goof Perfomance")
+elif nilai >=  60:
+    print("Average Perfomance")
+else:
+    print("Poor Performance") 

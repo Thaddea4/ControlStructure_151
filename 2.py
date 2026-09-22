3a5eb67ba9cd9d5117ebc3e537cc6528c0882f9e
Thaddea4 <adeyasyie@gmail.com>
2026-09-22 11:32:24 +0700

    menambahkan program menemukan angka terbesar

---
+++ 2.py
@@ -0,0 +1,12 @@
+angka1 = int(input("Masukkan angka pertama: "))
+angka2 = int(input("Masukkan angka kedua: "))
+angka3 = int(input("Masukkan angka ketiga: "))
+
+if angka1 > angka2 and angka1 > angka3:
+    print("Angka terbesar adalah:", angka1)
+elif angka2 > angka1 and angka2 > angka3:
+    print("Angka terbesar adalah:", angka2)
+elif angka3 > angka1 and angka3 > angka2:
+    print("Angka terbesar adalah:", angka3)
+else:
+    print("Tidak ada angka terbesar")
